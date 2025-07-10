36e750829870d3c92ae590751d149cd2b5a9a849
chenhaozhe <chenhaozhe1@huawei.com>
2025-07-10 20:36:04 +0300

    support qwen2.5 mindspore

---
+++ ms_example/offline_infer.py
@@ -0,0 +1,33 @@
+import os
+
+import sglang as sgl
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+os.environ['MF_MODEL_CONFIG'] = os.path.join(current_dir, 'predict_qwen2_5_7b_instruct_800l_A2.yaml') 
+
+def main():
+    llm = sgl.Engine(model_path="/home/ckpt/qwen2.5-7b-hf",
+                     device="cpu",
+                     load_format="mindspore",
+                     max_total_tokens=20000,
+                     disable_overlap_schedule=True,
+                     tp_size=1,
+                     dp_size=1)
+
+    prompts = [
+         "Hello, my name is",
+         "The president of the United States is",
+         "The capital of France is",
+         "The future of AI is",
+    ]
+
+    sampling_params = {"temperature": 0.01, "top_p": 0.9}
+
+    outputs = llm.generate(prompts, sampling_params)
+    for prompt, output in zip(prompts, outputs):
+        print("===============================")
+        print(f"Prompt: {prompt}\nGenerated text: {output['text']}")
+
+
+if __name__ == "__main__":
+    main()
--- python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
@@ -50,10 +50,13 @@ if _use_aiter:
 
 try:
     import vllm
-
     VLLM_AVAILABLE = True
 except ImportError:
     VLLM_AVAILABLE = False
+
+if VLLM_AVAILABLE and not (_is_cuda or _is_npu or (_is_cpu and _is_cpu_amx_available)):
+    from vllm import _custom_ops as vllm_ops
+    from vllm._custom_ops import scaled_fp8_quant
 
 logger = logging.getLogger(__name__)
 
